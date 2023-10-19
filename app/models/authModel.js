59ca7130defe5d309fb7ede37a680de0e595ba17
IceScout26 <dhimasscout@gmail.com>
2023-10-19 10:17:51 +0700

    fix : connection from authRoutes

---
+++ app/models/authModel.js
@@ -0,0 +1,19 @@
+const pool = require('../../config/queries'); // Pastikan sesuaikan dengan letak file Anda
+
+// Query functions for authentication
+const getUserByEmail = (email, callback) => {
+    pool.query('SELECT * FROM users WHERE email = $1', [email], callback);
+};
+
+const createUser = (email, password, gender, role, callback) => {
+    pool.query(
+        'INSERT INTO users (email, password, gender, role) VALUES ($1, $2, $3, $4)',
+        [email, password, gender, role],
+        callback
+    );
+};
+
+module.exports = {
+    getUserByEmail,
+    createUser,
+};
