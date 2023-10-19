d4fa06577ce10e6fa723415cb332e84e4030683f
IceScout26 <dhimasscout@gmail.com>
2023-10-19 10:17:21 +0700

    feat : authRoutes.js

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const pool = require('../../config/queries');
-const { TokenExpiredError } = require('jsonwebtoken'); // Import error TokenExpiredError (opsional)
+const authController = require('../controllers/authController'); // Import controller
+const { authorize } = require('../controllers/authController'); // Import authorize middleware
 
 /**
  * @swagger
@@ -21,7 +20,10 @@ const { TokenExpiredError } = require('jsonwebtoken'); // Import error TokenExpi
  *       properties:
  *         token:
  *           type: string
- *
+ */
+
+/**
+ * @swagger
  * /auth/login:
  *   post:
  *     summary: User login
@@ -40,27 +42,7 @@ const { TokenExpiredError } = require('jsonwebtoken'); // Import error TokenExpi
  *             schema:
  *               $ref: '#/components/schemas/AuthResponse'
  */
-router.post('/login', (req, res) => {
-    const { email, password } = req.body;
-
-    pool.query('SELECT * FROM users WHERE email = $1', [email], (error, results) => {
-        if (error) {
-            throw error;
-        }
-
-        if (results.rows.length === 1) {
-            const user = results.rows[0];
-            if (user.password === password) {
-                const token = jwt.sign({ email: email, id: user.id }, 'scipio', { expiresIn: '1h' });
-                res.json({ token });
-            } else {
-                res.status(401).json({ message: 'Gagal login' });
-            }
-        } else {
-            res.status(401).json({ message: 'Gagal login' });
-        }
-    });
-});
+router.post('/login', authController.loginUser);
 
 /**
  * @swagger
@@ -82,35 +64,7 @@ router.post('/login', (req, res) => {
  *             schema:
  *               $ref: '#/components/schemas/AuthResponse'
  */
-router.post('/register', (req, res) => {
-    const { email, password, gender, role } = req.body;
-
-    pool.query('SELECT MAX(id) AS max_id FROM users', (error, results) => {
-        if (error) {
-            throw error;
-        }
-
-        const maxId = results.rows[0].max_id || 0;
-        const newId = maxId + 1;
-
-        pool.query(
-            'INSERT INTO users (id, email, password, gender, role) VALUES ($1, $2, $3, $4, $5)',
-            [newId, email, password, gender, role],
-            (error, results) => {
-                if (error) {
-                    throw error;
-                }
-
-                const user = {
-                    email: email,
-                    id: newId
-                };
-                const token = jwt.sign(user, 'scipio', { expiresIn: '1h' });
-                res.json({ message: 'Pendaftaran berhasil', token });
-            }
-        );
-    });
-});
+router.post('/register', authController.registerUser);
 
 /**
  * @swagger
@@ -126,21 +80,8 @@ router.post('/register', (req, res) => {
  *       401:
  *         description: Unauthorized, token is missing or invalid.
  */
-function authorize(req, res, next) {
-    const token = req.header('x-auth-token');
-    if (!token) return res.status(401).json({ message: 'Akses ditolak. Token tidak disediakan.' });
+router.get('/authorize', authController.authorizeUser, (req, res) => {
+    res.status(200).json({ message: 'User has been authorized.' });
+});
 
-    try {
-        const decoded = jwt.verify(token, 'scipio');
-        req.user = decoded;
-        next();
-    } catch (ex) {
-        if (ex instanceof TokenExpiredError) {
-            res.status(401).json({ message: 'Token telah kadaluwarsa.' });
-        } else {
-            res.status(400).json({ message: 'Token tidak valid.' });
-        }
-    }
-}
-
-module.exports = { router, authorize };
+module.exports = router;
